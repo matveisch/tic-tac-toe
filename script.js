c4c2cb6b5ac5a58b3d9cee643926e06b136e8f2c
matveisch <matvesch@protonmail.com>
2021-10-08 12:36:30 +0400

    Signs appearing

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
 let gameBoard = (function() {
     'use strict';
+    const boardContainer = document.querySelector('.board-container');
 
-    let boardContent = ['X','O','X','X','O','X','O','O','X'];
+    let boardContent = ['','','','','','','','',''];
 
-    // создаем для каждого элемента в массиве отдельный div с этим элементом
-    boardContent.forEach((item) => {
-        const boardContainer = document.querySelector('.board-container');
-        const container = document.createElement('div');
-        container.classList.add('board-element');
-        container.textContent = item;
-        boardContainer.appendChild(container);
-    })
+    function displayBoard() {
+        // divCounter используется для создания id
+        let divCounter = 0;
+
+        // очищаем страницу каждый раз перед выводом div
+        while (boardContainer.firstChild) {
+            boardContainer.removeChild(boardContainer.firstChild)
+        }
+
+        // создаем для каждого элемента в массиве отдельный div с этим элементом
+        boardContent.forEach((item) => {
+            const container = document.createElement('div');
+
+            container.classList.add('board-element');
+
+            // создаем id для каждого div
+            container.id = divCounter;
+            divCounter++;
+
+            container.textContent = item;
+            boardContainer.appendChild(container);
+        })
+    }
+
+    displayBoard()
 
     return {
+        boardContent: boardContent,
+        displayBoard: displayBoard,
     };
 })();
 
-const playerFactory = (name, sign) => {
-    return {name, sign}
+let displayController = (function() {
+    'use strict';
+
+    const boardElements = document.querySelectorAll('.board-element');
+    boardElements.forEach((element) => {
+        element.addEventListener('click', () => {
+            gameBoard.boardContent.splice(1, 1, 'X');
+            gameBoard.displayBoard();
+
+        })
+    })
+
+    return {
+
+    };
+})();
+
+// создаем игроков и присваиваем каждому свой элемент
+const playerFactory = (sign) => {
+    return {sign}
 }
 
-const player1 = playerFactory('me', 'X');
-const player2 = playerFactory('you', 'O');
-
-console.log(player1);
-console.log(player2);
+const player1 = playerFactory('X');
+const player2 = playerFactory('O');
